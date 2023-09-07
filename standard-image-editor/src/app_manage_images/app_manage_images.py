4e9b60177068ac1464704f28ccf158b479cbb3af
Claudiu Ghit <claudiu.ghit@nokia.com>
2023-09-08 00:12:39 +0300

    commit manage 1

--- standard-image-editor/src/app_manage_images/app_manage_images.py
+++ standard-image-editor/src/app_manage_images/app_manage_images.py
@@ -1,7 +1,11 @@
-import json
+# packages
+import sys
 from flask import Flask, request
 from flask_lambda import FlaskLambda
 
+sys.path.append('../..')
+
+# local
 from src.common import common_functions as common, file_manager as fm
 from handlers import handler_manage_images
 
@@ -13,72 +17,38 @@ else:
     app = FlaskLambda(__name__)
 
 
-@app.route('/env-setup', methods=['POST'])
-def post_environment_setup() -> tuple:
-    common.log(post_environment_setup.__name__)
-    mandatory_parameters = ["environment"]
 
-    for mandatory_parameter in mandatory_parameters:
-        if mandatory_parameter not in request:
-            return common.respond_bad_request_missing(mandatory_parameter)
+@app.route('/post-image', methods=['POST'])
+def post_image() -> tuple:
+    common.log(post_image.__name__)
+     if 'file' not in request.files:
+            error = 'No file part'
+            common.log(error)
+            return common.respond_bad_request_missing('file')
+        file = request.files['file']
+        
+        if file.filename == '':
+            common.log('No selected file')
+            return common.respond_bad_request_missing('filename')
 
-    environment = request["environment"]
-
-    return ()
-
-
-@app.route('/post-images', methods=['POST'])
-def post_images(request: str) -> tuple:
-    req = json.loads(request)
-    common.log(post_images.__name__)
-
+    # save  to s3
     return handler_manage_images.add_images(req)
 
 
-@app.route('/get-images', methods=['GET'])
-def get_images(request: json) -> tuple:
-    common.log(get_images.__name__)
-    mandatory_parameters = ["image_paths"]
-
-    for mandatory_parameter in mandatory_parameters:
-        if mandatory_parameter not in request:
-            return common.respond_bad_request_missing(mandatory_parameter)
-
-    image_paths = request["image_paths"]
-
+@app.route('/get-image', methods=['GET'])
+def get_image() -> tuple:
+    common.log(get_image.__name__)
     return ()
 
 
-@app.route('/get-all-images', methods=['GET'])
-def get_all_images() -> tuple:
-    common.log(get_all_images.__name__)
-
-    return ()
-
-
-@app.route('/delete-images', methods=['DELETE'])
-def delete_images(request: json) -> tuple:
-    common.log(delete_images.__name__)
-    mandatory_parameters = ["image_paths"]
-
-    for mandatory_parameter in mandatory_parameters:
-        if mandatory_parameter not in request:
-            return common.respond_bad_request_missing(mandatory_parameter)
-
-    image_paths = request["image_paths"]
-
-    return ()
-
-
-@app.route('/delete-all-images', methods=['DELETE'])
-def delete_all_images() -> tuple:
-    common.log(delete_all_images.__name__)
-
+@app.route('/delete-image', methods=['DELETE'])
+def delete_image() -> tuple:
+    common.log(delete_image.__name__)
     return ()
 
 
 @app.route('/get-outputs', methods=['GET'])
-def get_outputs(request: json) -> tuple:
+def get_outputs() -> tuple:
     common.log(get_outputs.__name__)
     mandatory_parameters = ["output_paths"]
 
@@ -89,14 +59,6 @@ def get_outputs(request: json) -> tuple:
     output_paths = request["output_paths"]
 
     return ()
-
-
-@app.route('/get-all-outputs', methods=['GET'])
-def get_all_outputs() -> tuple:
-    common.log(get_all_outputs.__name__)
-
-    return ()
-
 
 if __name__ == '__main__':
     common.check_bucket()
